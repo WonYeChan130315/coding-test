952ab78bd5c0e9e04a1689f97edca1f312c7702d
WonYeChan1303 <wonyechan7772@gmail.com>
2024-01-30 10:54:30 +0900

    서울에서 김서방 찾기 24/1/30

--- obj/Debug/net8.0/c#_vscode.AssemblyInfo.cs
+++ obj/Debug/net8.0/c#_vscode.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("c#_vscode")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+89f14fe993770e013ba4b90baa484e0679016a44")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b8cf48bf33a83cd01faf6128f7d6ca441a374470")]
 [assembly: System.Reflection.AssemblyProductAttribute("c#_vscode")]
 [assembly: System.Reflection.AssemblyTitleAttribute("c#_vscode")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
