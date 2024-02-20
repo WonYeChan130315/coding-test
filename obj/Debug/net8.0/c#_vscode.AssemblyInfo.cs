441bf26c13f88b2f9966185e1edcca91aa45e9ac
WonYeChan1303 <wonyechan7772@gmail.com>
2024-02-20 13:53:41 +0900

    행렬의 덧셈 24/2/20

--- obj/Debug/net8.0/c#_vscode.AssemblyInfo.cs
+++ obj/Debug/net8.0/c#_vscode.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("c#_vscode")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+504a427e97e667dbc8acb6829ef027d79da8adc1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+01868dabd1ea7941d08c7561a6e0b26d4eecad78")]
 [assembly: System.Reflection.AssemblyProductAttribute("c#_vscode")]
 [assembly: System.Reflection.AssemblyTitleAttribute("c#_vscode")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
