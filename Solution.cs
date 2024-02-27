38cf4f27bda6540924dc9537956ea6eca199700f
WonYeChan1303 <wonyechan7772@gmail.com>
2024-02-27 14:35:30 +0900

    JadenCase 문자열 만들기 24/2/27

--- Solution.cs
+++ Solution.cs
@@ -2,29 +2,44 @@
 최댓값과 최솟값
 
 문제 설명:
-문자열 s에는 공백으로 구분된 숫자들이 저장되어 있습니다. str에 나타나는 숫자 중 최소값과 최대값을 찾아 이를 "(최소값) (최대값)"형태의 문자열을 반환하는 함수, solution을 완성하세요.
-예를들어 s가 "1 2 3 4"라면 "1 4"를 리턴하고, "-1 -2 -3 -4"라면 "-4 -1"을 리턴하면 됩니다.
+JadenCase란 모든 단어의 첫 문자가 대문자이고, 그 외의 알파벳은 소문자인 문자열입니다. 단, 첫 문자가 알파벳이 아닐 때에는 이어지는 알파벳은 소문자로 쓰면 됩니다. (첫 번째 입출력 예 참고)
+문자열 s가 주어졌을 때, s를 JadenCase로 바꾼 문자열을 리턴하는 함수, solution을 완성해주세요.
 
 제한 사항:
-> s에는 둘 이상의 정수가 공백으로 구분되어 있습니다.
+> s는 길이 1 이상 200 이하인 문자열입니다.
+
+> s는 알파벳과 숫자, 공백문자(" ")로 이루어져 있습니다.
+    숫자는 단어의 첫 문자로만 나옵니다.
+    숫자로만 이루어진 단어는 없습니다.
+    공백문자가 연속해서 나올 수 있습니다.
 */
+
+using System.Text;
 
 public class Solution {
     public string solution(string s) {
-        string[] str_nums = s.Split(' ');
+        bool space = true;
 
-        int max = int.MinValue;
-        int min = int.MaxValue;
+        for (int i = 0; i < s.Length; i++) {
+            if (s[i] == ' ')
+                space = true;
 
-        foreach(string num in str_nums) {
-            int n = int.Parse(num);
-            
-            if(max < n) 
-                max = n;
-            if(min > n)
-                min = n;
+            if (char.IsLetter(s[i])) {
+                StringBuilder sb = new StringBuilder(s);
+
+                if (space) {
+                    sb[i] = char.ToUpper(s[i]);
+                } else if (!space && char.IsUpper(s[i])) {
+                    sb[i] = char.ToLower(s[i]);
+                }
+
+                s = sb.ToString();
+            }
+
+            if (char.IsLetterOrDigit(s[i]))
+                space = false;
         }
 
-        return max + "" + min;
+        return s;
     }
 }
--- obj/Debug/net8.0/c#_vscode.AssemblyInfo.cs
+++ obj/Debug/net8.0/c#_vscode.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("c#_vscode")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea3fe89104dda0c17e7426d6ea038a10037b3d50")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a9172502f0ff00693381bd38e4f4e9e7c57fdf55")]
 [assembly: System.Reflection.AssemblyProductAttribute("c#_vscode")]
 [assembly: System.Reflection.AssemblyTitleAttribute("c#_vscode")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
