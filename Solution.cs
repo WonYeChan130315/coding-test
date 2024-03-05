2ffa67fc41e84a22146cbb60ee38eb478926e47c
WonYeChan1303 <wonyechan7772@gmail.com>
2024-03-05 18:49:59 +0900

    이진 변환 반복하기 24/3/5

--- Solution.cs
+++ Solution.cs
@@ -1,37 +1,39 @@
 ﻿/* 
-올바른 괄호
+이진 변환 반복하기
 
 문제 설명:
-괄호가 바르게 짝지어졌다는 것은 '(' 문자로 열렸으면 반드시 짝지어서 ')' 문자로 닫혀야 한다는 뜻입니다. 예를 들어
+0과 1로 이루어진 어떤 문자열 x에 대한 이진 변환을 다음과 같이 정의합니다.
 
-"()()" 또는 "(())()" 는 올바른 괄호입니다.
-")()(" 또는 "(()(" 는 올바르지 않은 괄호입니다.
-'(' 또는 ')' 로만 이루어진 문자열 s가 주어졌을 때, 문자열 s가 올바른 괄호이면 true를 return 하고, 올바르지 않은 괄호이면 false를 return 하는 solution 함수를 완성해 주세요.
+x의 모든 0을 제거합니다.
+x의 길이를 c라고 하면, x를 "c를 2진법으로 표현한 문자열"로 바꿉니다.
+예를 들어, x = "0111010"이라면, x에 이진 변환을 가하면 x = "0111010" -> "1111" -> "100" 이 됩니다.
+
+0과 1로 이루어진 문자열 s가 매개변수로 주어집니다. s가 "1"이 될 때까지 계속해서 s에 이진 변환을 가했을 때, 이진 변환의 횟수와 변환 과정에서 제거된 모든 0의 개수를 각각 배열에 담아 return 하도록 solution 함수를 완성해주세요.
 
 제한 사항:
-> 문자열 s의 길이 : 100,000 이하의 자연수
-> 문자열 s는 '(' 또는 ')' 로만 이루어져 있습니다.
+> s의 길이는 1 이상 150,000 이하입니다.
+> s에는 '1'이 최소 하나 이상 포함되어 있습니다.
 */
 
 using System;
-using System.Collections.Generic;
 
 public class Solution {
-    public bool solution(string s) {
-        Stack<char> solo = new Stack<char>();
-
-        foreach (char c in s) {
-            if (c == '(') {
-                solo.Push(c);
-            } else if (c == ')') {
-                if (solo.Count == 0 || solo.Peek() != '(') {
-                    return false;
+    public int[] solution(string s) {
+        int[] answer = new int[2];
+        
+        while (s != "1") {
+            int countZero = 0;
+            for (int i = 0; i < s.Length; i++) {
+                if (s[i] == '0') {
+                    countZero++;
                 }
-                solo.Pop();
             }
+            int length = s.Length - countZero;
+            s = Convert.ToString(length, 2);
+            answer[0]++;
+            answer[1] += countZero;
         }
-
-        return solo.Count == 0;
+        
+        return answer;
     }
 }
-// (())()    )()(
